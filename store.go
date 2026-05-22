f0aa6bb1080e31f6758ff13bb8cf55f8ee321161
Preet Patel <preetpatel1223@gmail.com>
2026-05-21 23:21:10 -0500

    feat: started store.go and store_test.go

---
+++ store.go
@@ -0,0 +1,27 @@
+package main
+
+import "io"
+
+type PathTransformFunc func(string) string
+
+type StoreOpts struct {
+	PathTransformFunc PathTransformFunc
+}
+
+var DefaultPathTransformFunc = func(key string) string {
+	return key
+}
+
+type Store struct {
+	StoreOpts
+}
+
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathName := key
+}
